5feddba03e6389d55f8f4fa2404d124f9ce78dc4
Sam Hossler <sam_hossler@outlook.com>
2025-03-06 15:33:27 -0500

    cat animation

--- game-dev-club-game/character_body_2d.gd
+++ game-dev-club-game/character_body_2d.gd
@@ -8,12 +8,16 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		anim.play("CatPlayerCharacter_Jump_From_Run_Animation")
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+		anim.play("CatPlayerCharacter_Jump_From_Run_Animation")
 	if Input.is_action_pressed("move_down"):
 		direction.y += 1
+		anim.play("CatPlayerCharacter_Jump_From_Run_Animation")
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
+		anim.play("CatPlayerCharacter_Jump_From_Run_Animation")
 
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
