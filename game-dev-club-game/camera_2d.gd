fda74ce64c550e7eb9f610d6909f848a58c806f6
Ethan <Ethan@gmail.com>
2025-03-13 15:05:19 -0400

    你好

---
+++ game-dev-club-game/camera_2d.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var speed: float = 500.0
+
+func _process(delta: float) -> void:
+	var direction := Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+	
+	position += direction * speed * delta
